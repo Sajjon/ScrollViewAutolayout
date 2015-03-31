e585e5ac1f58e5e00e5a8a6966c99b5cc3fd7d3d
Sajjon <alex.cyon@gmail.com>
2015-03-31 12:26:35 +0200

    Added scrollview autolayout test project

---
+++ ScrollViewAutolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewAutolayout
+//
+//  Created by Cyon Alexander (Ext. Netlight) on 31/03/15.
+//  Copyright (c) 2015 com.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "ScrollView Autolayout test"
+        self.navigationController?.setToolbarHidden(false, animated: false)
+    }
+}
+
